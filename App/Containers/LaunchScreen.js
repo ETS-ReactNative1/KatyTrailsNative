7e4956a6545eeb7a840a24fbe0ee81d2605fe9e5
ChristopherJamesN <nady.christopher@gmail.com>
2018-01-05 14:00:00 -0600

    Add a TrailsScreen

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
 import { ScrollView, Text, Image, View } from 'react-native'
 import DevscreensButton from '../../ignite/DevScreens/DevscreensButton.js'
+import API from '../Services/Api'
 
 import { Images } from '../Themes'
 
-// Styles
 import styles from './Styles/LaunchScreenStyles'
+
+/*
+constructor (props) {
+  super(props)
+  const dataObjects = []
+  const rowHasChanged = (r1, r2) => r1 !== r2
+  const sectionHeaderHasChanged = (s1, s2) => s1 !== s2
+  this.ds = new ListView.DataSource({rowHasChanged, sectionHeaderHasChanged})
+
+  this.state = {
+    dataSource: this.ds.cloneWithRowsAndSections(dataObjects)
+  }
+  this.getData()
+}
+
+getData = async () => {
+  const api = API.create()
+  const trails = await api.getTrails()
+  this.setState({
+    dataSource: this.ds.cloneWithRowsAndSections(trails.data)
+  })
+}
+*/
 
 export default class LaunchScreen extends Component {
   render () {
--- App/Services/Api.js
+++ App/Services/Api.js
@@ -2,7 +2,7 @@
 import apisauce from 'apisauce'
 
 // our "constructor"
-const create = (baseURL = 'https://api.github.com/') => {
+const create = (baseURL = 'https://hidden-reaches-61697.herokuapp.com/') => {
   // ------
   // STEP 1
   // ------
@@ -34,9 +34,7 @@ const create = (baseURL = 'https://api.github.com/') => {
   // Since we can't hide from that, we embrace it by getting out of the
   // way at this level.
   //
-  const getRoot = () => api.get('')
-  const getRate = () => api.get('rate_limit')
-  const getUser = (username) => api.get('search/users', {q: username})
+  const getTrails = () => api.get('https://katy-hiking-trails.herokuapp.com/trails.json')
 
   // ------
   // STEP 3
@@ -52,9 +50,7 @@ const create = (baseURL = 'https://api.github.com/') => {
   //
   return {
     // a list of the API functions from step 2
-    getRoot,
-    getRate,
-    getUser
+    getTrails
   }
 }
 
