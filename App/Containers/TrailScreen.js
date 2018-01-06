d2e7ecbfdb89c469f7b2482101842ef8293b7b2c
ChristopherJamesN <nady.christopher@gmail.com>
2018-01-06 13:54:20 -0600

    Create .env file.

--- App/Containers/TrailScreen.js
+++ App/Containers/TrailScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { ScrollView, Text, Button, Linking, TouchableOpacity, View } from 'react-native'
 import { connect } from 'react-redux'
+import MapView from 'react-native-maps';
 
 import styles from './Styles/TrailScreenStyle'
 
@@ -27,6 +28,14 @@ class TrailScreen extends Component {
           <TouchableOpacity style={styles.row} onPress={() => Linking.openURL(this.props.navigation.state.params.maplink).catch(err => console.error('An error occurred', err))}>
             <Text style={styles.boldLabel}>Link to Map</Text>
           </TouchableOpacity>
+          <MapView
+            initialRegion={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
         </ScrollView>
       </View>
     )
